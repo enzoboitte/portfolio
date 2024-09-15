fa558f80e04931e985f2e23aac20a92ddc7437cc
zegmano <en.boitte@apiged.fr>
2024-09-15 23:03:01 +0200

    Header annimation - footer css - assets

---
+++ src/js/index.js
@@ -0,0 +1,45 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const targetDiv = document.querySelector('#content_move');
+    const title = document.querySelector('#content_move > h1');
+    const zoomImage = document.getElementById('zoomImage');
+    const circleBackground = document.getElementById('circleBackground');
+    const header = document.querySelector('.header');
+    let maxScroll = window.innerHeight; // Hauteur de la fenêtre
+    let isZoomed = false;
+
+    let scaleBase = 0.75;
+    let scaleCurr = scaleBase;
+    title.style.transform = `scale(${scaleBase})`;
+
+    function onScroll() {
+        let scrollTop = window.scrollY;
+
+
+        const targetPosition = targetDiv.getBoundingClientRect();
+        const Position = header.getBoundingClientRect();
+        const windowHeight = window.innerHeight;
+
+        if (!isZoomed) {
+            if (Position.bottom > windowHeight) {
+                targetDiv.style.bottom = "0";
+                targetDiv.style.position = "fixed";
+                let scrollPercent = scrollTop / maxScroll;
+                let scale = 1 + scrollPercent * 1;
+
+                circleBackground.style.display = "block";
+                circleBackground.style.transform = `scale(${scale})`;
+
+                scale = 0.5 + scrollPercent * 0.8; // Zoom de 1 à 2
+                title.style.transform = `scale(${scale >= 1 ? 1 : (scale <= scaleBase ? scaleBase : scale)})`;
+            } else if (Position.bottom - targetPosition.bottom <= 0)
+            {
+                console.log("---------")
+                targetDiv.style.bottom = "0";
+                targetDiv.style.position = "absolute";
+                circleBackground.style.display = "none";
+            }
+        }
+    }
+
+    window.addEventListener('scroll', onScroll);
+});
